5cfda7a6eeee714ea5d143ecab78452813ce43c3
melmoore <melmoore@live.unc.edu>
2019-10-19 16:31:58 -0400

    figured out why the long and wide data frames weren't matching--some sorting code was catching only wowes, but not para wanderers, and others were catching para wanderers but not wowes. For now, have removed all wowes and para wanderers. Will come back to address if I should leave one or more of these in.
    Figured out how to generally create a start stop data set. have an extra row where values are NA--goes from 0 to 3rd instar, but counts 3rd instar as an event at the next time step. will need to figure out how to adjust this, but have the bare bones of the data set working.

--- R scripts/Ms-Cc_tv_surv_data-transform.R
+++ R scripts/Ms-Cc_tv_surv_data-transform.R
@@ -71,21 +71,53 @@ pbc2 <- tmerge(pbc2, pbcseq, id=id,
 
 #attempting to create a start,stop data set for use in survival analysis
 
-#sort out dead individuals
+#to create a stop start dataset, need a wide format and a long format that have the same unique IDs
+  ##problem is wowes, which sort out as dead in the wide dataframe, but not in the long data frame
+  ##some 30+/-10 bugs wandered, which are sorted out in the long data frame (because of the wowe column),
+  ##but not the wide.
+
+  ##for initial analysis, removing all "para" treatment bugs that wandered, because it is unknown if there
+  ##was a failed ovp, rescue effect, human error, or other that caused it.
+
+
+#sort out dead and para wandered individuals
 tv$date.died.j[is.na(tv$date.died.j)]<-0
 tv_cl <- subset(tv, date.died.j==0)
 
-tv_lng$date.died.j[is.na(tv_lng$date.died.j)]<-0
-tv_lngc <- subset(tv_lng, date.died.j==0)
+tv_cl$mass.wand[is.na(tv_cl$mass.wand)]<-0
+tv_cl$para_wand <- ifelse(tv_cl$treatment=="para" & tv_cl$mass.wand>0, 1, 0)
+tv_cl <- subset(tv_cl, para_wand==0)
+tv_cl$mass.wand[tv_cl$mass.wand==0]<-NA
+
+
+#make a sorting column for wowes
+tv_lng$date.em.j[is.na(tv_lng$date.em.j)]<-0
+tv_lng$wowe <- ifelse(tv_lng$temp.avg=="30" & tv_lng$temp.var=="10" & tv_lng$date.em.j=="0"
+                      & tv_lng$treatment=="para", 1, 0)
+tv_lng$date.em.j[tv_lng$date.em.j==0]<-NA
+
+tv_lngc <- subset(tv_lng, wowe==0)
+
+#making a sorting column for para wanderers
+tv_lngc$mass.wand[is.na(tv_lngc$mass.wand)]<-0
+tv_lngc$para_wand <- ifelse(tv_lngc$treatment=="para" & tv_lngc$mass.wand>0, 1, 0)
+tv_lngc <- subset(tv_lngc, para_wand==0)
+tv_lngc$mass.wand[tv_lngc$mass.wand==0]<-NA
+
 
 #making a sorting/event column to indicate the event of wandering or wasp emergence
 tv_lngc$end_evnt <- ifelse(tv_lngc$instar=="end", 1, 0)
 
+tv_cl$date.wand.j[is.na(tv_cl$date.wand.j)]<-0
+tv_cl$end_ev <- ifelse(tv_cl$date.wand.j>0, "wand", "em")
 
-tv_sel <- select(tv_cl, bug.id, treatment, temp.avg, temp.var, ttend)
 
-test <- tmerge(data1 = tv_sel, data2 = tv_lngc, id=bug.id, end = event(end_evnt))
-test <- tmerge(data1 = test, data2 = tv_lngc, id=bug.id, instr = tdc(day.age, instar))
+tv_sel <- select(tv_cl, bug.id, treatment, temp.avg, temp.var, ttend, end_ev)
+
+test <- tmerge(data1 = tv_sel, data2 = tv_sel, id=bug.id, end=event(ttend, end_ev))
+test <- tmerge(data1 = test, data2 = tv_lngc, id=bug.id, instar=tdc(day.age, instar))
+test <- tmerge(data1 = test, data2 = tv_lngc, id=bug.id, mass=tdc(day.age, mass))
+
 
 #running into problems because of mongos, need to sort better so can remove them from both dataframes
 
