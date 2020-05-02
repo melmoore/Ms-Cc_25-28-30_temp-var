36d33d25eb2a07fb6fadd05c5a4baee62c6d4520
melmoore <melmoore@live.unc.edu>
2020-05-02 11:32:44 -0400

    exploring more details of tvor data and cpt data to see if the 25 and 30 constant treatments were similar for wasp survival/dev time and host mass and dev time. Pretty similar

--- R scripts/Ms-Cc_tv-rep-orig_comp-const-temps_with-CxPxT.R
+++ R scripts/Ms-Cc_tv-rep-orig_comp-const-temps_with-CxPxT.R
@@ -366,3 +366,199 @@ wsem_re_mod_null <- glmer(cbind(num.em, num.unem) ~ 1 + (1|bug.id),
 anova(wsem_re_mod_null, wsem_re_mod1, wsem_re_mod_ta, wsem_re_mod_ld, wsem_re_mod_expt, 
       wsem_re_mod_tald, wsem_re_mod_taex, wsem_re_mod_ldex, wsem_re_mod_taldex, test="Chisq")
 
+
+
+#--------------------
+
+#Joel noted two points in cpt data that have much lower num.em at 30 than the rest of the data, suggested 
+#removing and seeing how that affected slope/difference between tv data
+
+which(ad_psub$load > 100 & ad_psub$num.em < 25)
+
+ad_psub2 <- ad_psub[-c(10, 43), ]
+
+
+#plot of num em by load, colored by expt, faceted by temp--two outliers removed
+com_wsnmem_plot2 <- ggplot(ad_psub2, aes(x=load, y=num.em, group=expt, color=expt))
+com_wsnmem_plot2 + geom_point(size=6
+) + geom_smooth(method = "lm"
+) + scale_color_manual(values=c("black", "#E69F00")
+) + facet_wrap(~temp.avg)
+
+
+#NUMBER EMERGED
+
+ws_num_sum <- summarySE(ad_psub2, measurevar = "num.em",
+                        groupvars = c("temp.avg", "expt"),
+                        na.rm = TRUE)
+ws_num_sum
+
+
+
+#PROP EMERGED
+
+ws_psem_sum <- summarySE(ad_psub2, measurevar = "ps.em",
+                         groupvars = c("temp.avg", "expt"),
+                         na.rm = TRUE)
+
+ws_psem_sum
+
+
+
+
+#quick and dirty analysis of wasp survival to emergence between the two experiments--two outliers removed
+
+wsem_mod2 <- glm(cbind(num.em, num.unem) ~ temp.avg * load * expt,
+                 family = binomial,
+                 data = ad_psub2,
+                 na.action=na.omit)
+anova(wsem_mod1)
+summary(wsem_mod1)
+
+
+
+#----------------------
+
+#effects on wasp development time
+
+#plot of time to emergence by load, colored by expt, faceted by temp--two outliers removed
+com_wsttem_plot2 <- ggplot(ad_psub2, aes(x=load, y=ttem.w, group=expt, color=expt))
+com_wsttem_plot2 + geom_point(size=6
+) + geom_smooth(method = "lm"
+) + scale_color_manual(values=c("black", "#E69F00")
+) + facet_wrap(~temp.avg)
+
+
+
+#box plot of time to emergence, colored by expt, faceted by temp--two outliers removed
+com_wsttem_boxplot <- ggplot(ad_psub2, aes(x=expt, y=ttem.w, group=expt, fill=expt))
+com_wsttem_boxplot + geom_boxplot(
+) + scale_color_manual(values=c("black", "#E69F00")
+) + facet_wrap(~temp.avg)
+
+
+#mean time to emergence
+ws_ttem_sum <- summarySE(ad_psub2, measurevar = "ttem.w",
+                        groupvars = c("temp.avg", "expt"),
+                        na.rm = TRUE)
+ws_ttem_sum
+
+
+#--------------------------------
+
+#combine wandering and emergence into end columm for preparation to convert data frame to long format
+
+#remove para treatment that wandered
+all_dat$date.wand[is.na(all_dat$date.wand)] <- 0
+all_dat$keep <- ifelse(all_dat$treatment=="para" & all_dat$date.wand > 0, 0, 1)
+
+all_dat <- subset(all_dat, keep==1)
+
+all_dat$date.wand[all_dat$date.wand==0] <- NA
+all_dat$keep <- NULL
+
+#make combined data set into long format to compare host growth and development time
+
+all_dat_sub <- subset(all_dat, temp.avg!=20)
+
+ad_lng <- gather(all_dat_sub, instar, mass, mass.3, mass.4, mass.5, mass.end)
+ad_lng$instar <- gsub("mass.", "", ad_lng$instar)
+ad_lng$instar <- gsub("48", "", ad_lng$instar)
+
+
+ad_age_lng <- gather(all_dat_sub, instar, age, tt3, tt4, tt5, ttend)
+ad_age_lng$instar <- gsub("tt", "", ad_age_lng$instar)
+ad_age_lng$instar <- gsub(".h", "", ad_age_lng$instar)
+
+
+ad_age_lng <- select(ad_age_lng, bug.id, temp.avg, temp.var, instar, age)
+
+ad_lng <- merge(ad_lng, ad_age_lng, by=c("bug.id", "temp.avg", "temp.var", "instar"))
+
+
+#remove the orig 30 data
+ad_lng$keep <- ifelse(ad_lng$temp.avg==30 & ad_lng$expt=="orig", 0, 1)
+
+ad_lng <- subset(ad_lng, keep==1)
+
+
+#rename orig and repl experiments to just be tvor
+ad_lng$expt <- ifelse(ad_lng$expt=="orig" | ad_lng$expt=="repl", "tvor", ad_lng$expt)
+
+
+#---------------------
+
+#plot of growth and development for hosts between temp avgs and expts
+
+#take log of mass
+ad_lng$log_mss <- log(ad_lng$mass)
+
+#means of mass and age
+lmss_sum <- summarySE(ad_lng, measurevar = "log_mss",
+                      groupvars = c("temp.avg", "treatment", "expt", "instar"),
+                      na.rm = TRUE)
+lmss_sum
+
+
+age_sum <- summarySE(ad_lng, measurevar = "age",
+                     groupvars = c("temp.avg", "treatment", "expt", "instar"),
+                     na.rm = TRUE)
+age_sum
+
+
+lmss_sum$age <- age_sum[ , 6]
+lmss_sum$age_se <- age_sum[ , 8]
+
+#plot mean mass and mean age for each temp.avg, treatment and expt
+mn_ma_plot <- ggplot(lmss_sum, aes(x=age, y=log_mss, group=expt, color=expt))
+mn_ma_plot + geom_point(size=6
+) + geom_line(size=2
+) + geom_errorbar(aes(ymin=log_mss - se, ymax=log_mss + se),
+                  width=.7, size=1.2
+) + geom_errorbarh(aes(xmin=age - age_se, xmax=age + age_se),
+                   height=.5, size=1.2
+) + facet_wrap(temp.avg~treatment)
+
+
+
+#-------------------------
+
+#calculate means and range of sample sizes for each treatment
+
+temp.avg <- c(25, 25, 25, 25, 
+              28, 28, 28, 28, 
+              30, 30, 30, 30)
+
+temp.var <- c(0, 10, 0, 10, 
+              0, 10, 0, 10,  
+              0, 10, 0, 10)
+
+treatment <- c("control", "control", "para", "para",
+               "control", "control", "para", "para",
+               "control", "control", "para", "para")
+
+n <- c(35, 32, 37, 38, 31, 26, 30, 24, 43, 42, 47, 31)
+
+
+ncalc <- data.frame(temp.avg, temp.var, treatment, n)
+
+View(ncalc)
+
+
+nsum_ta <- summarySE(ncalc, measurevar = "n",
+                  groupvars = "temp.avg")
+nsum_ta
+
+
+nsum_tv <- summarySE(ncalc, measurevar = "n",
+                     groupvars = "temp.var")
+nsum_tv
+
+
+nsum_trt <- summarySE(ncalc, measurevar = "n",
+                      groupvars = "treatment")
+nsum_trt
+
+
+range(ncalc$n)
+mean(ncalc$n)
